dd477218ae1830de8401ec47e84ed72e619584b2
devkhan <devesh.khandelwal@outlook.com>
2015-07-23 01:18:40 +0530

    revert to crossword

--- app/src/main/java/teamdapsr/loaders/app/MainActivity.java
+++ app/src/main/java/teamdapsr/loaders/app/MainActivity.java
@@ -1,24 +1,70 @@
 package teamdapsr.loaders.app;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener
 {
 
-	ProgressBar df;
 
-	@Override
+
+    private static final String[] ITEMS = {"Progressbar demo", "PiechartDemo", "Gif", "Crossword"};
+
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+		ListView list = new ListView(this);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ITEMS);
+
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(this);
+
+		setContentView(list);
+
 
 	}
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0: {
+                Intent i = new Intent(this, progressdemo.class);
+                startActivity(i);
+                break;
+            }
+            case 1: {
+                Intent i = new Intent(this, PieChardDemo.class);
+                startActivity(i);
+                break;
+            }
+            case 2: {
+                Intent i = new Intent(this, Gif.class);
+                startActivity(i);
+                break;
+            }
+            case 3: {
+                Intent i = new Intent(this, crosswordgrid.class);
+                startActivity(i);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
